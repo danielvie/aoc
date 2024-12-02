4c643f0900a127b591b6ccb9f536075de2d89342
Daniel Vieira <danielvie@gmail.com>
2024-12-02 00:56:25 -0300

    remove shortcuts in similarity search

--- 2024/aoc_zig_day1/src/main.zig
+++ 2024/aoc_zig_day1/src/main.zig
@@ -54,32 +54,16 @@ fn part1(left_values: *std.ArrayList(i32), right_values: *std.ArrayList(i32)) u3
 
 fn part2(left_values: *std.ArrayList(i32), right_values: *std.ArrayList(i32)) u32 {
     // compute similarity score
-    var left_: i32 = -1;
-    var count_: u32 = 0;
     var similarity: u32 = 0;
 
     for (left_values.items) |left| {
-        // if the left is the same as before, we dont need to search again
-        if (left_ == left) {
-            similarity += @abs(left) * count_;
-            continue;
-        }
-
         var count: u32 = 0;
         for (right_values.items) |right| {
-            // if the `right` is bigger than left, left will not be finded anymore
-            if (right > left) {
-                continue;
-            }
-
             if (left == right) {
                 count += 1;
             }
         }
         similarity += @abs(left) * count;
-
-        left_ = left;
-        count_ = count;
     }
 
     return similarity;
@@ -113,6 +97,7 @@ fn run(choice: FileChoice) !struct { distance: u32, similarity: u32 } {
 
     // running exercices
     const distance: u32 = part1(&sorted_left_values, &sorted_right_values);
+
     const similarity: u32 = part2(&sorted_left_values, &sorted_right_values);
 
     return .{
@@ -127,7 +112,7 @@ pub fn main() !void {
     std.debug.print("distance: {}\n", .{result_test.distance});
     std.debug.print("similarity: {}\n", .{result_test.similarity});
 
-    const result_run = try run(FileChoice.TEST);
+    const result_run = try run(FileChoice.RUN);
     std.debug.print("\nlist:\n", .{});
     std.debug.print("distance: {}\n", .{result_run.distance});
     std.debug.print("similarity: {}\n", .{result_run.similarity});
